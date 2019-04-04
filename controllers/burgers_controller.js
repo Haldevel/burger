9783753c185f5c21808a5f1db3287b9fd1f40d09
Susie <45341415+Haldevel@users.noreply.github.com>
2019-04-03 22:00:39 -0700

    start working on insert

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,10 +18,10 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, false], function (result) {
         // Send back the ID of the new burger
         res.json({ id: result.insertId });
-    });
+    });   
 });
 
 router.put("/api/burgers/:id", function(req, res) {
