9faa1d49b67538c5598a77e2530606557a35deb0
Susie <45341415+Haldevel@users.noreply.github.com>
2019-04-04 15:28:42 -0700

    add devour button logic

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name, devoured ) VALUES ('Fancy Eggplant Burger', false);
 INSERT INTO burgers (burger_name, devoured ) VALUES ('Grilled Salmon Burger', false);
-INSERT INTO burgers (burger_name, devoured ) VALUES ('Wild Mushrooms Burger', false);
+INSERT INTO burgers (burger_name, devoured ) VALUES ('Wild Mushrooms Burger', true);
