caa9048792ad64f1a183aee4f4a2738f7085351d
Susie <45341415+Haldevel@users.noreply.github.com>
2019-04-04 19:49:01 -0700

    add more comments

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,14 +1,13 @@
+//The front-end javascript file
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function () {
-  $(".change-devoured").on("click", function (event) {
+  $(".change-devoured").on("click", function (event) { //on click event handler allowing to set devoured property to true by clicking a button
     var id = $(this).data("id");
-    //var newSleep = $(this).data("newsleep");
-
     var devourToTrue = {
       devoured: true
     };
 
-    // Send the PUT request.
+    // Send the PUT request to update a burger 'devoured' property and set it to true
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: devourToTrue
@@ -21,15 +20,16 @@ $(function () {
     );
   });
 
-  $(".add-form").on("submit", function (event) {
+  $(".add-form").on("submit", function (event) { //event handler for Submit button which adds a new burger to the db
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
+    //read the name of a new burger
     var newBurger = {
       burger_name: $("#newb").val().trim()
     };
 
-    // Send the POST request.
+    //Send the POST request with the new burger name and set its devoured property to false
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
