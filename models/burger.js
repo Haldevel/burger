ac78b38d7423603ac374057e1091181b5ccb458a
Susie <45341415+Haldevel@users.noreply.github.com>
2019-04-04 19:59:36 -0700

    add even more comments

--- models/burger.js
+++ models/burger.js
@@ -1,20 +1,26 @@
-// Import the ORM object to create functions that will interact with the database.
+//The model for a burger
+//Import the ORM object to create functions that will interact with the database.
+
 var orm = require("../config/orm.js");
 
 //create the burger object (model)
 
 var burger = {
-    selectAll: function(cb) { 
+    //inside selectAll for burger model call selectAll orm function and pass it the table name the callback function
+    selectAll: function(cb) {  
       orm.selectAll("burgers", function(res) {
         cb(res);
       });
     },
-    // The variables cols and vals are arrays.
+    //inside the insertOne burger model function call the insertOne orm function and pass it the table
+    //name, the columns to insert and the corresponding values, and the callback function
     insertOne: function(cols, vals, cb) {
       orm.insertOne("burgers", cols, vals, function(res) {
         cb(res);
       });
     },
+    //inside the updateOne burger model function call the updateOne orm function and pass it the table name,
+    //the key-value pairs for columns and the values, the where clause condition and the callback function
     updateOne: function(objColVals, condition, cb) {
       orm.updateOne("burgers", objColVals, condition, function(res) {
         cb(res);
